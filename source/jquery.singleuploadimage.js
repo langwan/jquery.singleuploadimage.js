e522fcc704935f9dcdedc8aff6a6609e4cc74ed8
Martijn van der Meulen <martijn_vdm@hotmail.com>
2015-10-23 15:38:20 +0200

    Prevent empty upload
    The added if($('#' + settings.inputId).get(0).files[0] !== undefined) { } statement prevents an error and empty div to appear when the user clicks the 'cancel' button in the upload dialog box of their browser.

--- source/jquery.singleuploadimage.js
+++ source/jquery.singleuploadimage.js
@@ -32,32 +32,33 @@
         }, options);
 
         $('#' + settings.inputId).bind('change', function() {
-            $this.css('backgroundImage', 'none');
-            var fd = new FormData();
-            fd.append($('#' + settings.inputId).attr('name'), $('#' + settings.inputId).get(0).files[0]);
-
-            var xhr = new XMLHttpRequest();
-            xhr.addEventListener("load", function(ev) {
-                $this.html('');
-                var res = eval("(" + ev.target.responseText + ")");
-
-                if(res.code != 0) {
-                    settings.onError(res.code);
-                    return;
-                }
-                var review = ('<img src="'+res.url+'" style="width:'+$this.width()+'px;height:'+$this.height()+'px;"/>');
-                $this.append(review);
-                settings.onSuccess(res.url, res.data);
-
-            },
-            false);
-            xhr.upload.addEventListener("progress", function(ev) {
-                settings.OnProgress(ev.loaded, ev.total);
-            }, false);
-            
-            xhr.open("POST", settings.action, true);
-            xhr.send(fd);  
-
+            if($('#' + settings.inputId).get(0).files[0] !== undefined) {
+                $this.css('backgroundImage', 'none');
+                var fd = new FormData();
+                fd.append($('#' + settings.inputId).attr('name'), $('#' + settings.inputId).get(0).files[0]);
+    
+                var xhr = new XMLHttpRequest();
+                xhr.addEventListener("load", function(ev) {
+                    $this.html('');
+                    var res = eval("(" + ev.target.responseText + ")");
+    
+                    if(res.code != 0) {
+                        settings.onError(res.code);
+                        return;
+                    }
+                    var review = ('<img src="'+res.url+'" style="width:'+$this.width()+'px;height:'+$this.height()+'px;"/>');
+                    $this.append(review);
+                    settings.onSuccess(res.url, res.data);
+    
+                },
+                false);
+                xhr.upload.addEventListener("progress", function(ev) {
+                    settings.OnProgress(ev.loaded, ev.total);
+                }, false);
+                
+                xhr.open("POST", settings.action, true);
+                xhr.send(fd);
+            }
         });  
        
     	return this;
